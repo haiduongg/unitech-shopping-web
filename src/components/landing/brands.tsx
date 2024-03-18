aaae8ff4219136625cf6644334c843c6140ed399
Cao Hai Duong <46746869+haiduongg@users.noreply.github.com>
2024-03-18 22:02:44 +0700

    Complete home page

--- src/components/landing/brands.tsx
+++ src/components/landing/brands.tsx
@@ -1,43 +1,46 @@
+"use client"
 import React from 'react';
 
 import { IconType } from 'react-icons';
-import { SiApple } from "react-icons/si";
-import { SiXiaomi } from "react-icons/si";
-import { SiSamsung } from "react-icons/si";
-import { SiAsus } from "react-icons/si";
-import { SiAcer } from "react-icons/si";
-import { SiLenovo } from "react-icons/si";
-import { SiArstechnica } from "react-icons/si";
-import { SiBaidu } from "react-icons/si";
-import { SiItchdotio } from "react-icons/si";
+import {
+	SiApple,
+	SiXiaomi,
+	SiSamsung,
+	SiAsus,
+	SiAcer,
+	SiLenovo,
+	SiArstechnica,
+	SiBaidu,
+	SiItchdotio,
+} from 'react-icons/si';
 
-interface IBrands {
-	name: string,
-	logo: IconType,
-}
+import { InfiniteMovingCards } from '@/components/ui/infinite-moving-cards';
 
 export default function Brands() {
-	const brands: IBrands[] = [
-		{ name: 'Apple', logo: SiApple },
-		{ name: 'Xiaomi', logo: SiXiaomi },
-		{ name: 'Samsung', logo: SiSamsung },
-		{ name: 'Asus', logo: SiAsus },
-		{ name: 'Acer', logo: SiAcer },
-		{ name: 'Lenovo', logo: SiLenovo },
-		{ name: 'Arstechnica', logo: SiArstechnica },
-		{ name: 'Baidu', logo: SiBaidu },
-		{ name: 'Itchdotio', logo: SiItchdotio },
-	]
+	const brands: {
+		name: string;
+		logo: IconType;
+	}[] = [
+			{ name: 'Apple', logo: SiApple },
+			{ name: 'Xiaomi', logo: SiXiaomi },
+			{ name: 'Samsung', logo: SiSamsung },
+			{ name: 'Asus', logo: SiAsus },
+			{ name: 'Acer', logo: SiAcer },
+			{ name: 'Lenovo', logo: SiLenovo },
+			{ name: 'Arstechnica', logo: SiArstechnica },
+			{ name: 'Baidu', logo: SiBaidu },
+			{ name: 'Itchdotio', logo: SiItchdotio },
+		];
 
 	return (
 		<div className='py-16 bg-bgWhite'>
-			<ul className='animation-slider flex items-center justify-start gap-32 opacity-70'>
-				{brands.map((brand, index) => (
-					<li key={index}>
-						<brand.logo size={100} />
-					</li>
-				))}
-			</ul>
+			<div className="rounded-md flex flex-col antialiased dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+				<InfiniteMovingCards
+					items={brands}
+					direction="right"
+					speed="normal"
+				/>
+			</div>
 		</div>
 	);
 }
